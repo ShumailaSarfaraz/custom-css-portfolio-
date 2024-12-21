fa11b286ad9c22dbaaa8bdb673bee09f4acf8b08
Abdul Qadeer <shumailasarfaraz54@gmail.com>
2024-12-21 19:11:27 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+import Hero from "./Components/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect (() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay: 100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
